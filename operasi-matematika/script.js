c3dcc10d82bf207f6a3ed846c2e3a5f67a012b8f
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-04-20 14:06:26 +0900

    fix: operasi-matemateika, tambah spasi antar code

--- operasi-matematika/script.js
+++ operasi-matematika/script.js
@@ -6,10 +6,13 @@ let angka2 = 5;
 
 // Menjumlahkan angka1 dan angka2, hasilnya disimpan dalam variabel tambah
 let tambah = angka1 + angka2;
+
 // Mengurangkan angka2 dari angka1, hasilnya disimpan dalam variabel kurang
 let kurang = angka1 - angka2;
+
 // Mengalikan angka1 dengan angka2, hasilnya disimpan dalam variabel kali
 let kali = angka1 * angka2;
+
 // Membagi angka1 dengan angka2, hasilnya disimpan dalam variabel bagi
 let bagi = angka1 / angka2;
 
