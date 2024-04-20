725c2b174d2669188503e72053bb2cfb0ccd2c56
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-04-20 14:05:07 +0900

    fix: operasi-matematika, pisahkan html dan js

---
+++ operasi-matematika/script.js
@@ -0,0 +1,21 @@
+// Mendefinisikan variabel angka1 dengan nilai 10
+let angka1 = 10;
+
+// Mendefinisikan variabel angka2 dengan nilai 5
+let angka2 = 5;
+
+// Menjumlahkan angka1 dan angka2, hasilnya disimpan dalam variabel tambah
+let tambah = angka1 + angka2;
+// Mengurangkan angka2 dari angka1, hasilnya disimpan dalam variabel kurang
+let kurang = angka1 - angka2;
+// Mengalikan angka1 dengan angka2, hasilnya disimpan dalam variabel kali
+let kali = angka1 * angka2;
+// Membagi angka1 dengan angka2, hasilnya disimpan dalam variabel bagi
+let bagi = angka1 / angka2;
+
+// Menampilkan hasil operasi matematika
+// Fungsi console.log() digunakan untuk menampilkan output ke konsol browser
+console.log("Penjumlahan: " + tambah);
+console.log("Pengurangan: " + kurang);
+console.log("Perkalian: " + kali);
+console.log("Pembagian: " + bagi);
