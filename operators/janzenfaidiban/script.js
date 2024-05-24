a2029d96b6269ef1ccfd9784bdf794ae13fb841c
Janzen Faidiban <janzenfaidiban@gmail.com>
2024-05-24 20:14:42 +0900

    feat: tambah folder operators, dan file (index.html, script.js, style.css)

---
+++ operators/janzenfaidiban/script.js
@@ -0,0 +1,18 @@
+console.log('Hello SaCode!');
+
+document.write('Hello <b>Janzen</b>')
+
+var element = document.getElementById("hasil");
+element.innerHTML = 'Hello Papua!';
+
+// penerapan operator untuk membuat penjumlahan
+
+var penjumlahan = 5 + 6;
+var hasilPenjumlahan = document.getElementById('hasilPenjumlahan');
+hasilPenjumlahan.innerHTML = penjumlahan;
+
+// penerapan operator untuk membuat perkalian
+
+var perkalian = 5 * 6;
+var hasilPerkalian = document.getElementById('hasilPerkalian');
+hasilPerkalian.innerHTML = perkalian;
