edcfad80180c19144c4f886d8de566c592411ac4
Ricky <93272680+Rickyrizal@users.noreply.github.com>
2024-05-26 19:25:06 +0900

    feat : tambah folder operators dan file (index.html, script.js, style.css)

---
+++ operators/rickyedowai/script.js
@@ -0,0 +1,16 @@
+console.log("Hallo SaCode!");
+
+document.write("Hello <b>Ricky </b>");
+
+var element = document.getElementById("hasil");
+element.innerHTML = "Hello Papua!";
+
+// penerapan opetator untuk membuat penjumlahan
+var Penjumlahan = 5 + 6;
+var hasilPenjumlahan = document.getElementById("hasilPenjumlahan");
+hasilPenjumlahan.innerHTML = Penjumlahan;
+
+// penerapan operator untuk membuat perkalian
+var Perkalian = 5 * 6;
+var hasilPerkalian = document.getElementById("hasilPerkalian");
+hasilPerkalian.innerHTML = Perkalian;
