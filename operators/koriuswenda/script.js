64ecfea9224652ce46ad73acbfbec40d2eccff61
korius kussa wenda <koriuswenda@gmail.com>
2024-05-27 11:50:54 +0700

    feat: operators > koriuswenda file index.html, script.js style.css

---
+++ operators/koriuswenda/script.js
@@ -0,0 +1,19 @@
+console.log('Hello SaCode!');
+
+document.write('Hello<b>Korius </b>');
+
+var element = document.getElementById("hasil");
+element.innerHTML = 'Hello Papua !';
+
+//penerapan operator untuk membuat penjumlahan
+
+var penjumlahan = 5 + 6;
+var hasilpenjumlahan = document.getElementById('hasilpenjumlahan');
+hasilpenjumlahan.innerHTML =penjumlahan;
+
+
+//penerapan operator untuk membuat pembagian
+
+var perkalian = 5 * 6;
+var hasilperkalian = document.getElementById('hasilperkalian');
+hasilperkalian.innerHTML = perkalian;
